cc6871bcf8a1d60a7e8b34533b69d8ab5f1acf71
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-07-18 00:01:17 +0530

    Sci-kit implementation of kernel PCA using RBF

---
+++ Dimensionality Reduction/skPCAkernel.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_moons
+from sklearn.decomposition import KernelPCA
+
+X, Y = make_moons(n_samples=100, random_state=123)
+plt.scatter(X[Y==0, 0], X[Y==0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[Y==1, 0], X[Y==1, 1], color='blue', marker='o', alpha=0.5)
+plt.show()
+
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[Y==0, 0], X_skernpca[Y==0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[Y==1, 0], X_skernpca[Y==1, 1], color='blue', marker='o', alpha=0.5)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.show()
